6e5fd18291d699b4966b8e87ea8078b4c1f9075d
Tu Nguyen <im2nguyen@users.noreply.github.com>
2020-02-05 12:09:56 -0800

    anotha one

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
 resource "random_pet" "server" {}
+resource "random_pet" "server1" {}
 
 output "pet_name" {
     value = "${ random_pet.server }"
+}
+
+output "server1_pet_name" {
+    value = "${ random_pet.server1 }"
 }
